564d4d04adc2c5c67d6e46325eafb62a4d59ac9d
hectovs <htjvans@gmail.com>
2023-10-29 00:45:15 +0100

    hello world right align

---
+++ getting-started/example1/hello.cpp
@@ -0,0 +1,20 @@
+// hello.cpp: Maggie Johnson
+// Description: a program that prints the immortal saying "hello world"
+// print hello world 4 times on 6 lines where each statement is printed in a field of 17 spaces 
+
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+  for(int n=1; n<7 ; n++){
+    for(int i=1; i<5; i++){
+      std::cout << "Hello World!" <<  setw(17);
+    }
+    std::endl (std::cout);  
+  }
+
+  return 0;
+}
